06ff2df627b92618167607051f29e72fa4e10ed7
Gupta-Aryaman <aryaman28112002@gmail.com>
2022-11-17 23:01:35 +0530

    free a worker not wokring, rest done

--- main.cpp
+++ main.cpp
@@ -2,6 +2,17 @@
 #include "avl.cpp"
 using namespace std;
 
+std::ostream& bold_on(std::ostream& os)
+{
+    return os << "\e[1m";
+}
+
+std::ostream& bold_off(std::ostream& os)
+{
+    return os << "\e[0m";
+}
+
+
 
 int main(){
     // freopen("input.txt", "r", stdin);       //take input from input.txt file
@@ -21,6 +32,14 @@ int main(){
     int numberOfWorkersInACategory;
     string nameOfWorker;
     string phoneNumber;
+    Node* searchResult = NULL;
+
+    int choice5;
+
+    int z = 0;
+
+    vector<pair<pair<string, pair<Node *, Node *>>, Node*>> treesWithSelectedWorker;
+   // vector<pair<Node*, Node*>>::iterator iterator_treesWithSelectedWorker;
 
     vector<string> categories;
     vector<pair<string, vector<pair<string, pair<Node*, Node*>>> >> categoriesAlongWithSubcategoriesAndWorkers;
@@ -71,21 +90,110 @@ int main(){
         }
         //categoriesAlongWithSubcategoriesAndWorkers.resize(numberOfCategories);
         break;
-        
+
         //cout << line << endl;
     }
     
     // Close the File
     file_input.close();
 
-    // cout << (categoriesAlongWithSubcategoriesAndWorkers.size())<< endl;
-    vector<pair<string, vector<pair<string, pair<Node*, Node*>>> >>::iterator it;
-    // cout << numberOfCategories << endl;
-    for(it = categoriesAlongWithSubcategoriesAndWorkers.begin(); it < categoriesAlongWithSubcategoriesAndWorkers.end(); it++){
-        cout << 1 << endl;
+    // cout << getBalance((categoriesAlongWithSubcategoriesAndWorkers[0].second)[0].second.second);
+
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////////
+    begin:
+    int choice1;
+    cout <<endl<<bold_on<< "Categories" <<bold_off<<endl;
+    vector<pair<string, vector<pair<string, pair<Node*, Node*>>> >>::iterator it1; int x = 0;
+    for(it1 = categoriesAlongWithSubcategoriesAndWorkers.begin(); it1 < categoriesAlongWithSubcategoriesAndWorkers.end(); it1++){
+        cout << x++ <<". " <<(*it1).first << endl;
+    }
+    cout << "Enter your Choice: ";
+    cin >> choice1;
+
+    cout <<endl<<bold_on<< "Sub-categories" <<bold_off<<endl;
+    vector<pair<string, pair<Node*, Node*>>>::iterator it2; x = 0; int choice2;
+    for(it2 = categoriesAlongWithSubcategoriesAndWorkers[choice1].second.begin(); it2 < categoriesAlongWithSubcategoriesAndWorkers[choice1].second.end(); it2++){
+        cout << x++ <<". " <<(*it2).first << endl;
+    }
+    
+    cout << "Enter your Choice: ";
+    cin >> choice2;
+
+    if(categoriesAlongWithSubcategoriesAndWorkers[choice1].second[choice2].second.second!=NULL){
+        cout <<bold_on<< "Worker already selected from the category, select another category or free the worker first!" <<bold_off<< endl <<endl;
+        goto switchcase;
     }
 
-    cout << getBalance((categoriesAlongWithSubcategoriesAndWorkers[0].second)[0].second.first);
+    //pair<Node*, Node*>::iterator it2; x = 0;
+    // for(it2 = categoriesAlongWithSubcategoriesAndWorkers[choice].second.[choice].second.begin(); it2 < categoriesAlongWithSubcategoriesAndWorkers[choice].second.end(); it2++){
+    //     cout << x++ <<". " <<(*it2).first << endl;
+    // }
+    cout <<endl<<bold_on<< "Available Workers" <<bold_off<<endl;
+    inOrder(categoriesAlongWithSubcategoriesAndWorkers[choice1].second[choice2].second.first);
+    cout << endl;
+    cout <<endl<<bold_on<< "Non-Available Workers" <<bold_off<<endl;
+    inOrder(categoriesAlongWithSubcategoriesAndWorkers[choice1].second[choice2].second.second);
+
+    int choice3;
+    cout << "Select a worker by typing their id: ";
+    cin >> choice3;
     
+    searchResult = search(categoriesAlongWithSubcategoriesAndWorkers[choice1].second[choice2].second.first, choice3);
+    if(searchResult!=NULL){
+        categoriesAlongWithSubcategoriesAndWorkers[choice1].second[choice2].second.second = insert(categoriesAlongWithSubcategoriesAndWorkers[choice1].second[choice2].second.second, searchResult->key, searchResult->name, searchResult->phone_number);
+        categoriesAlongWithSubcategoriesAndWorkers[choice1].second[choice2].second.first = deleteNode(categoriesAlongWithSubcategoriesAndWorkers[choice1].second[choice2].second.first, choice3);
+    }
+    
+    treesWithSelectedWorker.push_back(make_pair(categoriesAlongWithSubcategoriesAndWorkers[choice1].second[choice2], categoriesAlongWithSubcategoriesAndWorkers[choice1].second[choice2].second.second));
+    cout <<endl<<bold_on<< "Selected By You:" <<bold_off<<endl;
+    inOrderForSelected(categoriesAlongWithSubcategoriesAndWorkers[choice1].second[choice2].second.second);
+    cout << endl;
+    //inOrder(categoriesAlongWithSubcategoriesAndWorkers[choice1].second[choice2].second.first);
+
+    switchcase:
+    cout << "1. Free a worker" << endl;
+    cout << "2. Show selected workers" << endl;
+    cout << "3. Select another worker" <<endl;
+    cout << "4. Exit" << endl;
+
+    int choice4;
+    cin >> choice4;
+    cout << endl;
+
+    switch(choice4){
+        case 1:
+            
+            for(auto & element:treesWithSelectedWorker){
+                cout << z++ <<". "<< element.second->name<< endl;
+            }
+            cout << "Enter the key to be deleted" << endl;
+            cin >> choice5;
+            cout << endl;
+
+            insert(treesWithSelectedWorker[choice5].first.second.first, treesWithSelectedWorker[choice5].first.second.second->key, treesWithSelectedWorker[choice5].first.second.second->name, treesWithSelectedWorker[choice5].first.second.second->phone_number);
+            treesWithSelectedWorker[choice5].first.second.second = NULL;
+            treesWithSelectedWorker.erase(find(treesWithSelectedWorker.begin(),treesWithSelectedWorker.end(),treesWithSelectedWorker[choice5]));
+
+            z = 0;
+            for(auto & element:treesWithSelectedWorker){
+                cout << z++ <<". "<< element.second->name<< endl;
+            }
+            goto switchcase;
+        case 2:
+            for(auto & element:treesWithSelectedWorker){
+                cout << "Category: " << (element.first.first) << " -> " <<bold_on<< element.second->name<<bold_off << ", " << element.second->phone_number << endl;
+            }
+            cout << endl;
+            goto switchcase;
+        case 3:
+            goto begin;
+        case 4:
+            break;
+        default:
+            cout << "Wrong choice\n";
+            break;
+    }
+
     return 0;
 }
