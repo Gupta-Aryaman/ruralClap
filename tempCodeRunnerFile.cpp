17928b8e191b0ef30292590667b319a205c92f1c
Gupta-Aryaman <aryaman28112002@gmail.com>
2022-11-23 17:12:32 +0530

    final

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,4 +1,4 @@
-z = 0;
-            // for(auto & element:arrWithSelectedWorker){
-            //     cout << z++ <<". "<< element.second->name<< endl;
-            // }
+if(searchResult!=NULL){
+        categoriesAlongWithSubcategoriesAndWorkers[choice1].second[choice2].second.second = insert(categoriesAlongWithSubcategoriesAndWorkers[choice1].second[choice2].second.second, searchResult->key, searchResult->name, searchResult->phone_number);
+        categoriesAlongWithSubcategoriesAndWorkers[choice1].second[choice2].second.first = deleteNode(categoriesAlongWithSubcategoriesAndWorkers[choice1].second[choice2].second.first, choice3);
+    }
